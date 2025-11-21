930c019866d3903aa97ada80ab4506a53e817b8d
unknown <lamaniboss2007@gamil.com>
2025-11-21 15:54:36 +0530

    added day 2 files

---
+++ day 2/memory_representation.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int num= -5;
+    float f=3.14;
+    printf("Integer:%d\n", num);
+    printf("Float:%f\n", f);
+
+    printf("Two's complement of -5 represented internally\n");
+    printf("Binary is stored as signed two's complement\n");
+
+    return 0;
+}
