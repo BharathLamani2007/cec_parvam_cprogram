b8213286225c20a771dd345558524fd7dac568ae
unknown <lamaniboss2007@gamil.com>
2025-11-21 14:53:06 +0530

    day2

--- day 2/switch.c
+++ day 2/switch.c
@@ -2,7 +2,7 @@
 int main()
 {
     int choice;
-    printf("1.add\n2. Subtract\n3. Multply\nEnter choice:");
+    printf("1.add\n2. Subtract\n3. Multply\n4. division\nEnter choice:");
     scanf("%d",&choice);
 
     switch(choice)
@@ -15,6 +15,9 @@ int main()
         break;
         case 3: 
         printf("You selected MULTPLY\n");
+        break;
+        case 4: 
+        printf("You selected DIVISION\n");
         break;
         default:
         printf("Invalid choice\n");
